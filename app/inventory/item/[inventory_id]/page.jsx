73eee5e3ec4b6dac52dd327cddd0f5f8e7afeb59
Kyle Greer <kylegreer.kg@gmail.com>
2024-05-08 19:19:22 -0400

    Live update individual item

--- app/inventory/item/[inventory_id]/page.jsx
+++ app/inventory/item/[inventory_id]/page.jsx
@@ -1,5 +1,5 @@
 import { createClient } from "@/utils/supabase/client";
-import { IndividualItem } from "@/app/inventory/item/individual-item";
+import { IndividualItem } from "../individual-item";
 
 async function getIndividualInventory(id) {
   const supabase = createClient();
@@ -18,21 +18,7 @@ export default async function IndividualItemPage({ params }) {
   const item = await getIndividualInventory(params.inventory_id);
   return (
     <div className="flex flex-1 flex-col overflow-y-auto">
-      <IndividualItem
-        nameOfItem={item[0].name}
-        category={item[0].category}
-        quantity={item[0].quantity}
-        price={item[0].price}
-        caseWeight={item[0].case_weight}
-        itemsPerCase={item[0].items_per_case}
-        duration={item[0].duration}
-        container={item[0].container}
-        barcode={item[0].barcode}
-        videoUrl={item[0].video_url}
-        exNumber={item[0].ex_number}
-        size={item[0].size}
-        id={item[0].inventory_id}
-      />
+      <IndividualItem item={item[0]} />
     </div>
   );
 }
--- app/inventory/item/individual-item.jsx
+++ app/inventory/item/individual-item.jsx
@@ -1,203 +1,163 @@
-"use client";
+"use server";
 
 import { CardTitle, CardHeader, CardContent, Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { useState } from "react";
-import { createClient } from "@/utils/supabase/client";
-import { useRouter } from "next/navigation";
+import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from "next/cache";
 
-export function IndividualItem({
-  nameOfItem,
-  category,
-  quantity,
-  price,
-  case_weight,
-  items_per_case,
-  duration,
-  container,
-  video_url,
-  size,
-  barcode,
-  ex_number,
-  notes,
-  id,
-}) {
-  const supabase = createClient();
-  const [name, setName] = useState(nameOfItem);
-  const [itemCategory, setCategory] = useState(category);
-  const [itemQuantity, setQuantity] = useState(quantity);
-  const [itemPrice, setPrice] = useState(price);
-  const [itemCaseWeight, setCaseWeight] = useState(case_weight);
-  const [itemItemsPerCase, setItemsPerCase] = useState(items_per_case);
-  const [itemDuration, setDuration] = useState(duration);
-  const [itemContainer, setContainer] = useState(container);
-  const [itemVideoUrl, setVideoUrl] = useState(video_url);
-  const [itemSize, setSize] = useState(size);
-  const [itemBarcode, setBarcode] = useState(barcode);
-  const [itemExNumber, setExNumber] = useState(ex_number);
-  const [itemNotes, setNotes] = useState(notes);
-  const router = useRouter();
-  const handleUpdate = async () => {
-    const { data, error } = await supabase
+export async function IndividualItem({ item }) {
+  const editItem = async (formData) => {
+    "use server";
+    const name = formData.get("name");
+    const category = formData.get("category");
+    const quantity = formData.get("quantity");
+    const price = formData.get("price");
+    const caseWeight = formData.get("case_weight");
+    const itemsPerCase = formData.get("items_per_case");
+    const duration = formData.get("duration");
+    const container = formData.get("container");
+    const videoURL = formData.get("video_url");
+    const size = formData.get("size");
+    const barcode = formData.get("barcode");
+    const exNumber = formData.get("ex_number");
+    const notes = formData.get("notes");
+    const supabase = createClient();
+    await supabase
       .from("inventory")
       .update({
         name: name,
-        category: itemCategory,
-        quantity: itemQuantity,
-        price: itemPrice,
-        case_weight: itemCaseWeight,
-        items_per_case: itemItemsPerCase,
-        duration: itemDuration,
-        container: itemContainer,
-        video_url: itemVideoUrl,
-        size: itemSize,
-        barcode: itemBarcode,
-        ex_number: itemExNumber,
-        notes: itemNotes,
+        category: category,
+        quantity: quantity,
+        price: price,
+        case_weight: caseWeight,
+        items_per_case: itemsPerCase,
+        duration: duration,
+        container: container,
+        video_url: videoURL,
+        size: size,
+        barcode: barcode,
+        ex_number: exNumber,
+        notes: notes,
       })
-      .eq("inventory_id", id);
-
-    if (error) {
-      console.log(error);
-    } else {
-      alert("Item updated successfully");
-      router.push("/");
-    }
+      .eq("inventory_id", item.inventory_id);
+    revalidatePath(`/inventory/item/${item.inventory_id}`);
   };
 
   return (
-    <div className="max-w-4xl mx-auto p-6">
-      <div className="flex items-center space-x-2 mb-6">
-        <Link href="/">
-          <ArrowLeftIcon className="h-5 w-5 text-muted-foreground" />
-        </Link>
-        <h1 className="text-2xl font-semibold">Edit {nameOfItem}</h1>
+    <form action={editItem}>
+      <div className="max-w-4xl mx-auto p-6">
+        {console.log(item)}
+        <div className="flex items-center space-x-2 mb-6">
+          <Link href="/">
+            <ArrowLeftIcon className="h-5 w-5 text-muted-foreground" />
+          </Link>
+          <h1 className="text-2xl font-semibold">Edit {item.name}</h1>
+        </div>
+        <Card className="divide-y min-w-[800px]">
+          <CardHeader>
+            <CardTitle>Product Details</CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="grid grid-cols-2 gap-x-8 gap-y-4">
+              <div>
+                <Label htmlFor="name">Name</Label>
+                <Input name="name" defaultValue={item.name ? item.name : ""} />
+              </div>
+              <div>
+                <Label htmlFor="category">Category</Label>
+                <Input
+                  name="category"
+                  defaultValue={item.category ? item.category : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="quantity">Quantity</Label>
+                <Input
+                  name="quantity"
+                  defaultValue={item.quantity ? item.quantity : 0}
+                />
+              </div>
+              <div>
+                <Label htmlFor="price">Price</Label>
+                <Input
+                  name="price"
+                  defaultValue={item.price ? item.price : 0}
+                />
+              </div>
+              <div>
+                <Label htmlFor="case_weight">Case Weight</Label>
+                <Input
+                  name="case_weight"
+                  defaultValue={item.case_weight ? item.case_weight : 0}
+                />
+              </div>
+              <div>
+                <Label htmlFor="items_per_case">Items Per Case</Label>
+                <Input
+                  name="items_per_case"
+                  defaultValue={item.items_per_case ? item.items_per_case : 0}
+                />
+              </div>
+              <div>
+                <Label htmlFor="video_url">Video URL</Label>
+                <Input
+                  name="video_url"
+                  defaultValue={item.video_url ? item.video_url : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="barcode">Barcode</Label>
+                <Input
+                  name="barcode"
+                  defaultValue={item.barcode ? item.barcode : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="ex_number">EX Number</Label>
+                <Input
+                  name="ex_number"
+                  defaultValue={item.ex_number ? item.ex_number : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="notes">Notes</Label>
+                <Input
+                  name="notes"
+                  defaultValue={item.notes ? item.notes : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="size">Size</Label>
+                <Input name="size" defaultValue={item.size ? item.size : 0} />
+              </div>
+              <div>
+                <Label htmlFor="duration">Duration</Label>
+                <Input
+                  name="duration"
+                  defaultValue={item.duration ? item.duration : ""}
+                />
+              </div>
+              <div>
+                <Label htmlFor="container">Container</Label>
+                <Input
+                  name="container"
+                  defaultValue={item.container ? item.container : ""}
+                />
+              </div>
+            </div>
+            <div className="flex justify-end mt-4 flex-cols-2 gap-2">
+              <Link href="/">
+                <Button variant="outline">Cancel</Button>
+              </Link>
+              <Button type="submit">Save</Button>
+            </div>
+          </CardContent>
+        </Card>
       </div>
-      <Card className="divide-y min-w-[800px]">
-        <CardHeader>
-          <CardTitle>Product Details</CardTitle>
-        </CardHeader>
-        <CardContent>
-          <div className="grid grid-cols-2 gap-x-8 gap-y-4">
-            <div>
-              <Label htmlFor="name">Name</Label>
-              <Input
-                id="name"
-                defaultValue={name}
-                onChange={(e) => setName(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="category">Category</Label>
-              <Input
-                id="category"
-                defaultValue={category}
-                onChange={(e) => setCategory(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="quantity">Quantity</Label>
-              <Input
-                id="quantity"
-                defaultValue={quantity}
-                onChange={(e) => setQuantity(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="price">Price</Label>
-              <Input
-                id="price"
-                defaultValue={price}
-                onChange={(e) => setPrice(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="case_weight">Case Weight</Label>
-              <Input
-                id="case_weight"
-                defaultValue={case_weight}
-                onChange={(e) => setCaseWeight(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="items_per_case">Items Per Case</Label>
-              <Input
-                id="items_per_case"
-                defaultValue={items_per_case}
-                onChange={(e) => setItemsPerCase(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="video_url">Video URL</Label>
-              <Input
-                id="video_url"
-                defaultValue={video_url}
-                onChange={(e) => setVideoUrl(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="barcode">Barcode</Label>
-              <Input
-                id="barcode"
-                defaultValue={barcode}
-                onChange={(e) => setBarcode(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="ex_number">EX Number</Label>
-              <Input
-                id="ex_number"
-                defaultValue={ex_number}
-                onChange={(e) => setExNumber(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="notes">Notes</Label>
-              <Input
-                id="notes"
-                defaultValue={notes}
-                onChange={(e) => setNotes(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="size">Size</Label>
-              <Input
-                id="size"
-                defaultValue={size}
-                onChange={(e) => setSize(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="duration">Duration</Label>
-              <Input
-                id="duration"
-                defaultValue={duration}
-                onChange={(e) => setDuration(e.target.value)}
-              />
-            </div>
-            <div>
-              <Label htmlFor="container">Container</Label>
-              <Input
-                id="container"
-                defaultValue={container}
-                onChange={(e) => setContainer(e.target.value)}
-              />
-            </div>
-          </div>
-        </CardContent>
-      </Card>
-      <div className="flex justify-end mt-4 flex-cols-2 gap-2">
-        <Link href="/">
-          <Button variant="outline">Cancel</Button>
-        </Link>
-        <Button variant="default" onClick={handleUpdate}>
-          Save
-        </Button>
-      </div>
-    </div>
+    </form>
   );
 }
 
